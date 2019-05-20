112b7eace67569a70e8abaf0a10c6b805cfa0cb9
fitszjerald <hesamya.ahrari@gmail.com>
2019-05-20 22:57:16 +0430

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Title from './components/Title';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.css';
+const API_TOKEN = '34deb606163c72f42440c915fc116f0f';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      tempreature:'',
+      country:'',
+      city:'',
+      weather:""
+    }
+  }
+  
+  // componentDidMount(){
+      getData = (e) =>{
+      e.preventDefault()
+          const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_TOKEN}`)
+      .then(data=>data.json())
+      .then(result=>{
+      this.setState({
+        tempreature:result.main.temp,
+        country:result.sys.country,
+        city:result.name,
+        weather:result.weather[0].description
+      })
+      })
+    }
+  // }
+
+  // getData = async (e) => {
+  //   e.preventDefault();
+    
+  //   const city = e.target.elements.city.value
+  //   const country = e.target.elements.country.value
+  //   const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_TOKEN}`)
+  //   const result =await api_call.json()
+  //   console.log(result)
+  //   this.setState({
+  //     tempreature:result.main.temp,
+  //     country:result.sys.country,
+  //     city:result.name
+  //   })
+  // }
+  render() {
+    return (
+      <div className="main-bg">
+      <div className='container app-bg'>
+        <div className='row justify-content-center'>
+
+          <div className='color-bg col-lg-6'>
+            <div className=' title-bg'>
+              <Title />
+            </div>
+          </div>
+          <div className='col-lg-6'>
+            <div className='weather-get'>
+            <Form getData={this.getData} />
+              <Weather 
+                tempreature={this.state.tempreature}
+                city={this.state.city}
+                country={this.state.country}
+                weather={this.state.weather}
+              />
+            
+            </div>
+          </div>          
+        </div>
+      </div>
+     </div>
+    )
+  }
 }
 
 export default App;
